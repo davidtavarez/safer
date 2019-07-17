16fff9c2e38de11c380e1f63cc3bc3d40a0141cc
David Tavarez <davidftavarez@gmail.com>
2019-07-17 09:44:34 -0400

    removing unused lines

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'Deleting cache...'
-sudo rm -Rf ./migrations/ ./src/safer.db ./.pytest_cache ./__pycache__ src/.pytest_cache src/__pycache__ src/views/__pycache__ src/helpers/__pycache__
-
 app="safer"
 docker build -t ${app} .
 docker run --rm -d -p 56733:80 --name=${app} -v $PWD:/app ${app}
