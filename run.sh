811ad7c0930f6457305781a986bd116371a4a753
David Tavarez <david@punksec.com>
2019-07-19 08:24:40 -0400

    improving a little bit

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 
 echo 'Installing requirements...'
 pip install -r ./requirements.txt
-echo 'Running migrate.sh'
 chmod u+x ./migrate.sh && ./migrate.sh
-echo 'Running test.sh'
 chmod u+x ./test.sh && ./test.sh
